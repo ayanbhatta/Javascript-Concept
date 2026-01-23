65ca3fd159369587ebaa385e85db412add012bf7
ayanbhatta <ayanb8127@gmail.com>
2026-01-23 16:25:07 +0530

    bom(open and close)

--- bom.js
+++ bom.js
@@ -1,77 +1,119 @@
 //?console object:
-var a=10
-console.log(a);
-console.log(window.a);
+// var a=10
+// console.log(a);
+// console.log(window.a);
 
-window.console.log("hello")
-console.error('I am error');
-console.warn("Warning")
+// window.console.log("hello")
+// console.error('I am error');
+// console.warn("Warning")
 
-let obj = {
-    name:"Ayan",
-    age:22
+// let obj = {
+//     name:"Ayan",
+//     age:22
     
-}
+// }
 
-console.table(obj)
+// console.table(obj)
 
-console.time("forLoop")
-for (let i = 0; i < 100; i++) {
+// console.time("forLoop")
+// for (let i = 0; i < 100; i++) {
    
     
-}
-console.timeEnd("forLoop")
-console.time("whileLoop")
-let i=0
-while(i<1000){
-    i++;
-}
-console.timeEnd("whileLoop")
+// }
+// console.timeEnd("forLoop")
+// console.time("whileLoop")
+// let i=0
+// while(i<1000){
+//     i++;
+// }
+// console.timeEnd("whileLoop")
 
 //?location
-console.log(location);
-console.log(location.hostname);
-console.log(location.port)
-console.log(location.protocol)
-location.relode()
+// console.log(location);
+// console.log(location.hostname);
+// console.log(location.port)
+// console.log(location.protocol)
+// location.reload()
 
-window.location="amazon"    //history stored
-location.href="google"//history stored
-location.assign("https://www.redbus.in/")//history stored
-location.replace("https://www.google.com")  //history not stored
+// window.location="amazon"    //history stored
+// location.href="google"//history stored
+// location.assign("https://www.redbus.in/")//history stored
+// location.replace("https://www.google.com")  //history not stored
 
 //? navigator:
-console.log(navigator);
-console.log(navigator.appName);
-console.log(navigator.appCodeName);
+// console.log(navigator);
+// console.log(navigator.appName);
+// console.log(navigator.appCodeName);
 
 //?screen
-console.log(screen);
-console.log(innerHeight);
-console.log(outerHeight);
-console.log(innerWidth);
-console.log(outerWidth);
+// console.log(screen);
+// console.log(innerHeight);
+// console.log(outerHeight);
+// console.log(innerWidth);
+// console.log(outerWidth);
 
-//?history
 
 //?window methods:
-//?prompt()
-let a = Number(prompt("value of a","234"))//2nd argument is default val.
-console.log(a);
-console.log(typeof a);
+//!prompt() alsop called dialogue
+// let a = Number(prompt("value of a","234"))//2nd argument is default val.
+// console.log(a);
+// console.log(typeof a);
 
 //?alert()
-alert("this is warning")
+// alert("this is warning")
 
 //?confirm()
-let value = confirm("are you sure you want to exit")
-console.log(value);
+// let value = confirm("are you sure you want to exit")
+// console.log(value);
 
 //?setTimeout(callback, delay)
-let timeId = setTimeout(() =>{
-    console.log("I am set timeout");
-},1000)
+// let timeId = setTimeout(() =>{
+//     console.log("I am set timeout");
+// },1000)
 
 //? clearInterval(id)
-clearInterval(interval)
+// clearInterval(interval)
 
+//? history
+// console.log(history);
+// console.log(history.back());
+// console.log(history.forward());
+// console.log(history.go());
+
+//? Navigator
+// console.log(navigator);
+// console.log(navigator.appName);
+// console.log(navigator.appCodeName);
+// console.log(navigator.onLine);
+// console.log(navigator.userAgent);
+
+//?screen
+// console.log(screen);
+// console.log(screen.width);
+// console.log(screen.height);
+
+// console.log(innerHeight);
+// console.log(outerHeight);
+// console.log(innerWidth);
+
+
+//? open() & close()
+// var newPage = open("https://www.google.com","_blank","height=500px,width=500px")
+// var anotherPage=open("https://www.google.com")
+// setTimeout(()=>{
+//     newPage.close()
+// },5000)
+
+//?using html page
+var newpge;
+function newPage1(){
+    newpge= open("https://www.google.com","_blank","height=500,width=500")
+}
+
+function closePage(){
+    if (newpge) {
+        newpge.close();
+    } else {
+        console.log("No window has been opened yet!");
+    }
+}
---
+++ practiceSet.js
@@ -0,0 +1,38 @@
+let arr=[1,[2,3],4,[5,[6,7]],8]
+let newArr=arr.flat(3)
+// console.log(newArr);
+const a=newArr.map(n => {
+    if(n%2===0){
+        return n**2
+    }if(n%2!==0){
+       return n**3
+    }
+})
+// console.log(a);
+let sum=0
+const arr2=newArr.filter(n=>{
+    if(n%5==0)
+        sum+=n
+    return n%5==0
+})
+// console.log(arr2);
+// console.log(sum);
+
+
+
+// let nums=[1,2,3]
+// nums.forEach(n=>console.log('F',n))
+// console.log("------");
+// nums.map(n=>n*2)
+// console.log("----------");
+// nums.filter(n=>n>1)
+// console.log("End");
+
+function test(cb){
+    console.log('A');
+    cb();
+    console.log('B');
+}
+test(()=>{
+    console.log('C');
+})
