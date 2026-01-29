e9e97ee8eab7425bfa9d92729cb98ba5e8a417ad
ayanbhatta <ayanb8127@gmail.com>
2026-01-29 16:37:42 +0530

    dom manipulation

--- practiceSet.js
+++ practiceSet.js
@@ -1,20 +1,20 @@
 let arr=[1,[2,3],4,[5,[6,7]],8]
 let newArr=arr.flat(3)
 // console.log(newArr);
-const a=newArr.map(n => {
-    if(n%2===0){
-        return n**2
-    }if(n%2!==0){
-       return n**3
-    }
-})
+// const a=newArr.map(n => {
+//     if(n%2===0){
+//         return n**2
+//     }if(n%2!==0){
+//        return n**3
+//     }
+// })
 // console.log(a);
-let sum=0
-const arr2=newArr.filter(n=>{
-    if(n%5==0)
-        sum+=n
-    return n%5==0
-})
+// let sum=0
+// const arr2=newArr.filter(n=>{
+//     if(n%5==0)
+//         sum+=n
+//     return n%5==0
+// })
 // console.log(arr2);
 // console.log(sum);
 
@@ -28,11 +28,15 @@ const arr2=newArr.filter(n=>{
 // nums.filter(n=>n>1)
 // console.log("End");
 
-function test(cb){
-    console.log('A');
-    cb();
-    console.log('B');
-}
-test(()=>{
-    console.log('C');
-})
+// function test(cb){
+//     console.log('A');
+//     cb();
+//     console.log('B');
+// }
+// test(()=>{
+//     console.log('C');
+// })
+
+//? create h1 tag with text "Hellow JavaScript". append "from JSpiders" to that text without append method
+
+//? create 3 div with common class - "js". Access them and add unique text of each class
---
+++ script.js
@@ -0,0 +1,65 @@
+// let ele = document.getElementById("heading1")
+// console.log(ele);
+// // console.dir(ele);
+// ele.style.background = "yellow";
+
+let elements = document.getElementsByClassName("heading2")
+console.log(elements);
+// console.dir(elements);
+elements[0].style.fontSize = "60px"
+elements[1].style.color = "blue"
+
+for(item of elements){
+    item.style.color="blue"
+    item.style.backgroundColor="yellow"
+    item.style.fontSize="10px"
+}
+
+//tagname
+// let parahs = document.getElementsByTagName('p')
+// console.log(parahs);
+
+//?querySelector('.class/#id/tag')
+// let parahs2 = document.querySelector('.heading2')
+// console.log(parahs2);
+
+//?querySelectorAll('.class/#id/tag')
+// let parahs3 = document.querySelectorAll('.heading2')
+// console.log(parahs3);
+
+// let heading = document.querySelector("h1")
+// console.log(heading.innerText);
+// console.log(heading.innerHTML);
+// heading.innerText="JS"
+// heading.innerHTML="<h6>javascript</h6>";
+// console.log(heading.innerText);
+// console.log(heading.textContent);
+
+//? DOM Attributes
+// let ele = document.querySelector('h1')
+// console.log(ele.getAttribute("name"));
+// ele.setAttribute("name","js")
+// ele.removeAttribute("name")
+
+// let button = document.createElement("button")
+// button.append("Click Me")
+
+// let myul = document.querySelector("div")
+// myul.append(button)
+// myul.append("button")
+
+// myul.appendChild(button)
+// myul.prepend(button)
+// myul.after(button)
+// myul.before(button)
+// myul.removeChild(button)
+
+for (let i = 1; i <= 3; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'js';
+    document.body.appendChild(newDiv);
+}
+const divs = document.querySelectorAll('.myclass');
+divs.forEach((div, index) => {
+    div.textContent = `I am unique div number ${index + 1}`;
+});
