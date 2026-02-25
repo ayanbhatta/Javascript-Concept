a18319f1ec8297829e2207e31a5f3bd0c9feeb7f
ayanbhatta <ayanb8127@gmail.com>
2026-02-25 22:16:54 +0530

    trick questions

---
+++ mock.js
@@ -0,0 +1,28 @@
+a()
+let a =()=>{
+    console.log("Hi");
+}
+function a(){
+    console.log("hello");
+}
+a()
+
+// bar();
+// let bar = function() {
+//   console.log("Hello!");
+// };
+
+
+// console.log(foo);
+// var foo = 1;
+// function foo() {
+//   return 2;
+// }
+a()
+var a =()=>{
+    console.log("Hi");
+}
+function a(){
+    console.log("hello");
+}
+a()
